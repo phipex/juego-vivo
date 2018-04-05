1f54d412a398e0ebfe28dd479ae341af2b55115a
desarrollo-pc <aphipex@gmail.com>
2018-04-05 14:28:16 -0500

    refactor de reload

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@ const connection = new WebSocket('ws://localhost:9876');
 const webview = document.querySelector('webview');
 const balance = document.querySelector('#balance-val');
 const URL = 'https://doradobet.com/productos/betshop/';
-//const URL = 'https://doradobet.com/productos/betshop2/';
+// const URL = 'https://doradobet.com/productos/betshop2/';
 const URLBASE = 'http://localhost:8080/rutavivo';
-//const URLBASE = 'http://192.168.1.240:8080/rutavivo';
-const URLBALANSE =  URLBASE + '/api/counters/machine/balance';
-const URLCASHOUT = URLBASE + "/api/even-logs/machine/cashout";
+// const URLBASE = 'http://192.168.1.240:8080/rutavivo';
+const URLBALANSE = `${URLBASE}/api/counters/machine/balance`;
+const URLCASHOUT = `${URLBASE}/api/even-logs/machine/cashout`;
 
 console.log(webview);
 
@@ -15,20 +15,19 @@ setInterval(getBalance, 1000);
 
 webview.addEventListener('did-navigate-in-page', getBalance);
 
-//did-navigate-in-page
+// did-navigate-in-page
 
-//webview.addEventListener('close', )
+// webview.addEventListener('close', )
 
-//webview.addEventListener('console-load-commit', getBalance);
-//webview.addEventListener('finish-load', getBalance);
-//webview.addEventListener('dom-ready', getBalance);
-//webview.addEventListener('dom-ready', getBalance);
+// webview.addEventListener('console-load-commit', getBalance);
+// webview.addEventListener('finish-load', getBalance);
+// webview.addEventListener('dom-ready', getBalance);
+// webview.addEventListener('dom-ready', getBalance);
 
 // When the connection is open, send some data to the server
 connection.onopen = function onopen() {
-  //connection.send('Ping'); // Send the message 'Ping' to the server
+  // connection.send('Ping'); // Send the message 'Ping' to the server
   getBalance();
-
 };
 
 // Log errors
@@ -41,6 +40,10 @@ connection.onmessage = function onmessage(e) {
   console.log(`Server: ${e.data}`);
   gestionMensaje(e.data);
 };
+
+function restart() {
+  webview.loadURL(URL);
+}
 
 function gestionMensaje(msg) {
   const NEWBALANCE = 'NEWBALANCE';
@@ -55,8 +58,8 @@ function gestionMensaje(msg) {
     }
     case RESTART: {
           // TODO reiniciar todo
+      restart();
 
-      webview.loadURL(URL);
       break;
     }
     case COBRAR: {
@@ -79,61 +82,59 @@ function gestionMensaje(msg) {
 }
 
 
-
 const numberWithCommas = (x) => {
   const parts = x.toString().split('.');
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return parts.join('.');
 };
 
-function getBalance(){
-  var data = null;
+function getBalance() {
+  const data = null;
 
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
 
-  xhr.addEventListener("readystatechange", function () {
+  xhr.addEventListener('readystatechange', function () {
     if (this.readyState === 4) {
       console.log(this.responseText);
-      var balacenObject = JSON.parse(this.responseText);
-      if(balacenObject.balance){
+      const balacenObject = JSON.parse(this.responseText);
+      if (balacenObject.balance) {
         console.log(balacenObject.balance);
         balance.innerHTML = numberWithCommas(balacenObject.balance);
       }
     }
   });
 
-  xhr.open("GET", URLBALANSE);
-  //xhr.open("GET", "http://192.168.1.240:8181/api/counters/machine/balance");
-  xhr.setRequestHeader("Cache-Control", "no-cache");
-  //xhr.setRequestHeader("Postman-Token", "1e55cbfb-eebe-4205-95bb-0fe4f5c89237");
+  xhr.open('GET', URLBALANSE);
+  // xhr.open("GET", "http://192.168.1.240:8181/api/counters/machine/balance");
+  xhr.setRequestHeader('Cache-Control', 'no-cache');
+  // xhr.setRequestHeader("Postman-Token", "1e55cbfb-eebe-4205-95bb-0fe4f5c89237");
 
   xhr.send(data);
 }
 
-function cashout(){
+function cashout() {
   connection.send('CASHOUT');
   requestCashout();
 }
 
-function requestCashout(){
-  var data = "vivo";
+function requestCashout() {
+  const data = 'vivo';
 
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
 
-  xhr.addEventListener("readystatechange", function () {
+  xhr.addEventListener('readystatechange', function () {
     if (this.readyState === 4) {
       console.log(this.responseText);
       getBalance();
     }
   });
 
-  xhr.open("POST", URLCASHOUT);
-  xhr.setRequestHeader("Cache-Control", "no-cache");
-  //xhr.setRequestHeader("Postman-Token", "95a4698c-e493-4bc7-ae41-5ad1f0649e72");
+  xhr.open('POST', URLCASHOUT);
+  xhr.setRequestHeader('Cache-Control', 'no-cache');
+  // xhr.setRequestHeader("Postman-Token", "95a4698c-e493-4bc7-ae41-5ad1f0649e72");
 
   xhr.send(data);
-
 }
 
